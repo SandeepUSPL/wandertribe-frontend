f35b2e50d64ee8b000c8ac11be3f68614bb9721b
SandeepUSPL <62245613+SandeepUSPL@users.noreply.github.com>
2020-06-13 14:18:41 +0530

    Add files via upload

---
+++ trekPackage.php
@@ -0,0 +1,227 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>WanderPackage</title>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-149144948-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'UA-149144948-1');
+    </script>
+    <link rel="icon" href="new_img/logo/logo3.png" type="image/gif">
+    <meta charset="utf-8">
+    <meta name="keywords" content="wandertribe ,theroadlesstravelled ,TRLT ,travel ,travelling ,traveltheworld ,naturalworld ,lifeawayfromlife ,outdoorliving ,incredibleindia ,travelindia ,travelrealindia ,exploreindia ,himachal ,himalaya ,pahadipeople ,pahadilife ,localpeople ,himalayanpeople ,adventure ,trekking ,hiking ,walking ,diving ,snorkeling ,running ,mountainbike ,birdwatching ,swimming ,fishing ,boating ,newplace ,newexperiences , ,travelphotography ,travelblogger ,travelgram ,tourism ,hotel ,hotels ,hospitalityindustry ,travelagency ,worldtravel ,traveltips ,nature ,beauty ,beautiful ,pahadi_corner ,himachal ,himachaltourism ,travelrealindia ,himachalpradesh ,photographers_of_india ,himalayas ,travelgram ,mountains ,nature ,wanderlust ,traveller ,portrait ,mypixeldiary ,traveldiaries ,Instahimachal ,StoriesofIndia ,indiapictures ,hills ,IndianPhotoGraphy ,weuttarakhand ,roads ,uttarakhandheaven ,roadtrip ,travelrealindia ,uttarakhand ,himachal   ,uttarkashidiaries ,himalayas  ,india ,travel ,mountains ,wanderlust,travelphotography ,travelcouple ,travelph ,travellers ,traveleurope ,travelblog ,travelstoke ,travelgram ,travelawesome ,travelguide ,traveller ,instatravel ,travel_drops ,travelbook ,travels ,traveler ,travelislife ,travelholic ,traveldeeper ,travelbag ,traveladdict ,travelling ,travelphoto ,traveltheworld ,traveling ,travelblogger ,travel_captures,solotravels ,solotraveldiaries ,solotravelstories ,nomadiclife ,womenwhoexplore ,womenwhotravel ,travelingladies ,familytravels ,travelingwithkids ,familytravelmoment ,roamtheplanet,sky ,clouds ,beach ,food ,nature  ,snow ,sunset ,night ,mountains ,sunsetporn ,nightphotography ,postcardsfromtheworld ,choosemountains ,optoutside ,ipulledoverforthis ,viewfromthetop ,sunset_madness,travelista  ,travelingplanet ,travelingworld ,travelingislife ,travelingsoul ,travelpicsdaily  ,travelstories ,globelletravels ,travelvibes ,globetrotters ,travelworld ,travelwriter ,adventureawaits  ,travellingram ,travelquote ,travelquotes ,theuprootedrose,exploretheglobe ,travelinstyle ,adventureanywhere ,letsgoeverywhere ,worldtraveller ,hello_worldpics ,travelwell ,postcardplaces ,meettheworld ,globetrotting,travelinspo">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+    <link rel="stylesheet" type="text/css" media="screen and (min-width: 1084px)" href="css/trekPackage.css">
+    <link rel="stylesheet" type="text/css" media="screen and (min-width: 601px) and (max-width: 1083px)" href="css/trekPackage600up.css">
+    <link rel="stylesheet" type="text/css" media="screen and (max-width: 600px)" href="css/trekPackage600down.css">
+
+    <link href="https://fonts.googleapis.com/css?family=Muli:300,400,600,700,900&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" type="text/css" href="jquery.datetimepicker.min.css">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script type="text/javascript" src="jquery.datetimepicker.full.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+    <script type="text/javascript" src="js/script.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/humanity/jquery-ui.css">
+    <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/humanity/jquery-ui.css">
+
+    
+</head>
+
+<body>
+    <!--  commonWBR -->
+    <?php include "commonWBR.php";?>
+    <!--commonWBR-->
+    <!--  loader -->
+
+    <div id="headerNew" style="background: #024959 0% 0% no-repeat padding-box;">
+        <div class="row headerNew">
+            <div class="col-xs-5 col-sm-2 logo">
+                <img src="new_img/logo/logo1.png" alt="">
+            </div>
+            <div class="col-xs-3 col-sm-2" style="margin-top: 22px;">
+                <a href="#">Trek overview</a>
+            </div>
+            <div class="col-xs-3 col-sm-2" style="margin-top: 22px;">
+                <a href="#">Itinerary</a>
+            </div>
+            <div class="col-xs-3 col-sm-3 col-sm-offset-1" style="margin-top: 20px;">
+                <h3>₹ 8,500 <span style="font-size: 20px; font-weight: 500;">Per adult</span></h3>
+            </div>
+            <div class="col-xs-3 col-sm-2" style="margin-top: 15px;">
+                <button>Book Now</button>
+            </div>
+        </div>
+    </div>
+
+    <div class="trek-pagination">
+        <h2>Treks > Himachal > Kasol Trek > <span>Package Details</span></h2>
+    </div>
+
+    <div class="packProgress">
+        <div>
+            <div id="circle1"></div>
+            <hr>
+            <div id="circle2"></div>
+            <hr>
+            <div id="circle2"></div>
+        </div>
+
+        <div class="row">
+            <div class="col-sm-4" style="padding: 0;">
+                <h3 style="opacity: 1;">Package details</h3>
+            </div>
+            <div class="col-sm-4">
+                <h3 style="text-align: center;">Traveller Information</h3>
+            </div>
+            <div class="col-sm-4">
+                <h3>Payment</h3>
+            </div>
+        </div>
+    </div>
+
+    <div class="row packDetail">
+        <div class="col-sm-4 form-query">
+            <div>
+                <h4>Number of people in a trek</h4>
+                <div class="restrict-people row">
+                    <h5 class="col-xs-6">Min - 8</h5>
+                    <h5 class="col-xs-6" style="text-align: right;">Max - 15</h5>
+                </div>
+                <form>
+                    <div>
+                        <select id="sel">
+							<option>Choose your date</option>
+						</select>
+                    </div>
+                    <div class="input-group">
+                        <span>Adult</span>
+                        <img src="new_img/icons/minus-circle.png" id="minusCircle">
+                        <div class="people-number">
+                            <input type="text" name="noOfPeople" id="people" placeholder="0">
+                        </div>
+                        <img src="new_img/icons/plus-circle.png" id="plusCircle">
+                    </div>
+                    <button type="submit">Book Now</button>
+                </form>
+                <h3>Have Any Question?</h3>
+                <h3>Contact us @</h3>
+                <h2>+91-9872-129281</h2>
+            </div>
+        </div>
+        <!-- <div class="col-sm-3 form-query">
+			<div>
+				<form>
+					<div class="input-group">
+						<input type="text" name="chooseDate" id="dateChosen" placeholder="Choose your date">
+					</div>
+					<div class="input-group">
+						<span>Adult</span>
+						<img src="icons/plus-circle.png" id="plusCircle">
+						<input type="text" name="noOfPeople" id="people" placeholder="0">
+						<img src="icons/minus-circle.png" id="minusCircle">
+					</div>
+					<button type="submit">Update Changes</button>
+				</form>
+				<h3>Have Any Question?</h3>
+				<h3>Contact us @</h3>
+				<h2>+91-9872-129281</h2>
+			</div>
+		</div> -->
+
+        <div class="col-sm-8 packPlace">
+            <h1></h1>
+
+            <div class="row">
+                <div class="col-xs-3 weather1">
+                    <img src="icons/sunny.png">
+                    <span>4 Days</span>
+                </div>
+                <div class="col-xs-3 weather1">
+                    <img src="icons/moon.png">
+                    <span>3 Nights</span>
+                </div>
+                <div class="col-xs-5 weather1">
+                    <img src="icons/destination.png">
+                    <span>Himachal Pradesh</span>
+                </div>
+            </div>
+
+            <h2></h2>
+
+            <div class="accomodation">
+                <h3>Accommodation with Activities</h3>
+
+                <div class="row">
+                    <div class="col-xs-4">
+                        <h4>Camping with Meals and Activities</h4>
+                    </div>
+                    <div class="col-xs-5">
+                        <h4>Price break down</h4>
+                        <h4 style="font-weight: 600; margin-top: 25px;" id="price-breakdown"></h4>
+                    </div>
+                    <div class="col-xs-3">
+                        <h4>Total Price</h4>
+                        <h4 style="font-weight: 600; margin-top: 25px;" id="total-price"></h4>
+                    </div>
+                </div>
+
+                <hr>
+
+                <h5></h5>
+            </div>
+            <a href="#"><button>Proceed</button></a>
+        </div>
+    </div>
+
+    <?php include 'commonFooter.php' ?>
+    <script type="text/javascript" src="js/trekPackage.js"></script>
+
+</body>
+
+<script>
+    $('body').click(function() {
+        $('.packPlace button').css('background-color', '#540a05');
+        $('.packPlace button').css('opacity', 0.2);
+        setTimeout(function() {
+            $('.packPlace button').css({
+                'background-color': '#FF5700',
+                'opacity': 1
+            });
+        }, 500);
+
+    })
+
+    //$('#dateChosen').datetimepicker();
+
+    $('#plusCircle').click(function() {
+        var val = $('#people').attr('placeholder');
+        var num = Number(val) + 1;
+        val = String(num);
+        document.getElementById("people").placeholder = val;
+    })
+
+    $('#minusCircle').click(function() {
+        if ($('#people').attr('placeholder') > 0) {
+            document.getElementById("people").placeholder =
+                String(Number($('#people').attr('placeholder')) - 1);
+        }
+    })
+</script>
+
+</html>
